bd1b79acfaefff48a259f6bdeb98dc6d2d324cbb
Mike Kroutikov <mkroutikov@innodata.com>
2021-08-31 14:42:16 -0400

    debugging async weirdness 5

--- index.js
+++ index.js
@@ -6,24 +6,20 @@ function sleep(howlong) {
 }
 
 (async function() {
-    try {
-        const directory          = core.getInput('directory');
-        const namePattern        = core.getInput('name-pattern');
-        const targetPattern      = core.getInput('target-pattern');
-        const acl                = core.getInput('acl');
-        const awsAccessKeyId     = core.getInput('aws-access-key-id');
-        const awsSecretAccessKey = core.getInput('aws-secret-access-key');
+    const directory          = core.getInput('directory');
+    const namePattern        = core.getInput('name-pattern');
+    const targetPattern      = core.getInput('target-pattern');
+    const acl                = core.getInput('acl');
+    const awsAccessKeyId     = core.getInput('aws-access-key-id');
+    const awsSecretAccessKey = core.getInput('aws-secret-access-key');
 
-        const [bucket, key] = await main(directory, namePattern, targetPattern, acl, awsAccessKeyId, awsSecretAccessKey);
+    const [bucket, key] = await main(directory, namePattern, targetPattern, acl, awsAccessKeyId, awsSecretAccessKey);
 
-        core.setOutput('location-s3', `s3://${bucket}/${key}`);
-        core.setOutput('location-http', `https://s3.amazonaws.com/${bucket}/${key}`);
+    core.setOutput('location-s3', `s3://${bucket}/${key}`);
+    core.setOutput('location-http', `https://s3.amazonaws.com/${bucket}/${key}`);
 
-        core.info(`Sleeping... ${new Date()}`);
+    core.info(`Sleeping... ${new Date()}`);
 
-        await sleep(20000);
-        core.info(`Done... ${new Date()}`);
-    } catch(error) {
-        core.setFailed(error.message);
-    }
-})().catch(console.log);
+    await sleep(20000);
+    core.info(`Done... ${new Date()}`);
+})().catch(core.setFailed(error.message));
